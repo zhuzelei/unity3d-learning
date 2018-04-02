86d17c1fbbe15ba882d397eec552d2a3968c2d32
zhuzelei <31604203+zhuzelei@users.noreply.github.com>
2018-04-02 20:06:08 +0800

    Add files via upload

---
+++ homework2/SolarSystem/RotateBeh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBeh : MonoBehaviour {
+
+    public Transform Planet;
+    public Transform Origin;
+    public float speed;
+    public float AroundSpeed;
+    public float AroundY, AroundZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 axis = new Vector3(0, AroundY, AroundZ);
+        Planet.transform.RotateAround(Origin.position, axis, AroundSpeed * Time.deltaTime);
+        Planet.transform.Rotate(Vector3.up * speed * Time.deltaTime);
+	}
+}
---
+++ homework2/SolarSystem/SunBeh.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunBeh : MonoBehaviour {
+
+    public Transform Sun;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Sun.Rotate(Vector3.up * 10 * Time.deltaTime);
+	}
+}
