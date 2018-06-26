20fb965394b48612b4f2c6a7cd2516f34a7d7076
zhuzelei <31604203+zhuzelei@users.noreply.github.com>
2018-06-26 22:44:11 +0800

    2018/6/26

---
+++ homework10/TankOnline/Assets/AssetsZZL/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<TankMovement>();
+        if (hitPlayer != null)
+        {
+            // Subscribe and Publish model may be good here!
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(30);
+
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ homework10/TankOnline/Assets/AssetsZZL/Combat.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Combat : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+
+    [SyncVar]
+    public int health = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        Debug.Log("haveIn");
+        if (!isServer)
+            return;
+
+        health -= amount;
+        if (health <= 0)
+        {
+            health = maxHealth;
+
+            // called on the server, will be invoked on the clients
+            RpcRespawn();
+        }
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
+        }
+    }
+}
